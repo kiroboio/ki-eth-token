d58f22bfbe0f2e2a95ff34cd85f236ec5739d228
Yura Sherman <yurasherman+github@gmail.com>
2020-08-03 19:42:46 +0300

    Ledger test

--- manual/test/ledger.js
+++ manual/test/ledger.js
@@ -19,6 +19,14 @@ const {
   advanceTimeAndBlock,
 } = require("../../test/lib/utils");
 
+const { hashMessage } = require('../../test/lib/hash');
+
+
+const getPrivateKey = (address) => {
+  const wallet = web3.currentProvider.wallets[address.toLowerCase()]
+  return `0x${wallet._privKey.toString('hex')}`
+}
+
 contract("Ledger Test", async (accounts) => {
   let ethApp, token, pool;
   let _web3;
@@ -29,10 +37,14 @@ contract("Ledger Test", async (accounts) => {
   const user2 = accounts[4];
   const user3 = accounts[5];
 
+  const user0 = accounts[0];
+
   const val1 = web3.utils.toWei("0.5", "gwei");
   const val2 = web3.utils.toWei("0.4", "gwei");
   const val3 = web3.utils.toWei("0.3", "gwei");
   const valBN = web3.utils.toBN("0");
+
+  const PATH = "44'/60'/0'/0/0";
 
   before("checking constants", async () => {
     assert(typeof tokenOwner == "string", "tokenOwner should be string");
@@ -47,21 +59,23 @@ contract("Ledger Test", async (accounts) => {
   });
 
   before("setup contract for the test", async () => {
-    token = await Token.new({ from: tokenOwner });
-    pool = await Pool.new(token.address, { from: poolOwner });
-    await token.disableTransfers(false, { from: tokenOwner });
 
-    mlog.log("web3           ", web3.version);
-    mlog.log("token contract ", token.address);
-    mlog.log("pool contract  ", pool.address);
-    mlog.log("tokenOwner     ", tokenOwner);
-    mlog.log("poolOwner      ", poolOwner);
-    mlog.log("user1          ", user1);
-    mlog.log("user2          ", user2);
-    mlog.log("user3          ", user3);
-    mlog.log("val1           ", val1);
-    mlog.log("val2           ", val2);
-    mlog.log("val3           ", val3);
+    mlog.log('web3           ', web3.version)
+    mlog.log('tokenOwner     ', tokenOwner)
+    mlog.log('poolOwner      ', poolOwner)
+    mlog.log('user0          ', user0)
+    mlog.log('user1          ', user1)
+    mlog.log('user2          ', user2)
+    mlog.log('user3          ', user3)
+    mlog.log('val1           ', val1)
+    mlog.log('val2           ', val2)
+    mlog.log('val3           ', val3)
+
+    token = await Token.deployed()
+    pool = await Pool.deployed()
+
+    mlog.log('token contract ', token.address)
+    mlog.log('pool contract  ', pool.address)
   });
 
   before("setup ledger", async () => {
@@ -69,94 +83,98 @@ contract("Ledger Test", async (accounts) => {
     ethApp = new App(transport);
   });
 
-  const sanitizeHex = (hex) => {
-    hex = hex.substring(0, 2) === "0x" ? hex.substring(2) : hex;
-    if (hex == "") return "";
-    return "0x" + padLeftEven(hex);
-  };
-
-  const getBufferFromHex = (hex) => {
-    hex = sanitizeHex(hex);
-    const _hex = hex.toLowerCase().replace("0x", "");
-    return new Buffer(_hex, "hex");
-  };
-
-  const toBuffer = (v) => {
-    if (ethUtil.isHexString(v)) {
-      return ethUtil.toBuffer(v);
-    }
-    return Buffer.from(v);
-  };
-
-  const padLeftEven = (hex) => {
-    hex = hex.length % 2 != 0 ? "0" + hex : hex;
-    return hex;
-  };
-
-  const msgSigner = async (msg, path) => {
-    const result = await ethApp.signPersonalMessage(
-      path,
-      toBuffer(msg).toString("hex")
-    );
-    const v = parseInt(result.v, 10) - 27;
-    const vHex = sanitizeHex(v.toString(16));
-    // return Buffer.concat([
-    //   getBufferFromHex(result.r),
-    //   getBufferFromHex(result.s),
-    //   getBufferFromHex(vHex),
-    // ]);
-    return {
-      v: vHex,
-      r: result.r,
-      s: result.s,
-    };
-  };
-
-  it("should be able to generate message", async () => {
-    const PATH = "44'/60'/0'/0/0";
-    await token.mint(pool.address, val1, { from: tokenOwner });
-    await token.mint(user1, val2, { from: tokenOwner });
-    await token.approve(pool.address, val3, { from: user1 });
-    await pool.deposit(val3, { from: user1 });
-    const { address } = await ethApp.getAddress(PATH);
-    mlog.log("ledger address", address);
-    const message = await pool.generateAcceptTokensMessage(address, 200, {
-      from: poolOwner,
-    });
+  it("should be able to generate, validate and execute 'token accept' message", async () => {
+    await token.mint(pool.address, val1, { from: tokenOwner })
+    const secret = Buffer.from("my secret")
+    const { address: ldgAddress } = await ethApp.getAddress(PATH);
+    mlog.log("ledger address", ldgAddress);
+    await pool.issueTokens(ldgAddress, 500, web3.utils.sha3(secret), { from: poolOwner })
+    const message = await pool.generateAcceptTokensMessage(ldgAddress, web3.utils.sha3(secret), { from: poolOwner })
     mlog.log("message", message);
-    mlog.log("sha3", web3.utils.sha3(message));
 
-    const toSign = web3.utils.sha3(message).slice(2);
-    mlog.log("message to sign", toSign);
-    const signed = await ethApp.signPersonalMessage(
+    const toSign = Buffer.from(web3.utils.sha3(message).slice(2)).toString('hex');
+    mlog.log("toSign", toSign);
+    const signedLedger = await ethApp.signPersonalMessage(
       PATH,
-      web3.utils.sha3(message).slice(2)
+      toSign,
     );
 
-    // const signed2 = await msgSigner(web3.utils.sha3(message).slice(2), PATH);
+    mlog.log('signed Ledger manual', JSON.stringify(signedLedger));
 
-    mlog.log(`signed: ${JSON.stringify(signed)}`);
+    let v = '0x' + signedLedger.v.toString(16);
 
-    let v = signed.v - 27;
-    v = v.toString(16);
-    if (v.length < 2) {
-      v = "0x0" + v;
-    }
+    let { r, s } = signedLedger;
 
-    let { r, s } = signed;
+    const hashedToSign = hashMessage(toSign);
+
+    const recovered = web3.eth.accounts.recover({
+      messageHash: hashedToSign,
+      v,
+      r: "0x" + r,
+      s: "0x" + s
+    })
+    mlog.log("Recovered Ledger", recovered);
 
     mlog.log("validating", `v is ${v} r is ${"0x" + r} s is ${"0x" + s}`);
 
     assert(
       await pool.validateAcceptTokensMessage(
-        address,
+        ldgAddress,
+        web3.utils.sha3(secret),
+        v,
+        "0x" + signedLedger.r,
+        "0x" + signedLedger.s,
+        { from: ldgAddress }
+      ),
+      "invalid ledger signature"
+    );
+    await pool.acceptTokens(ldgAddress, secret, v, "0x" + signedLedger.r, "0x" + signedLedger.s, { from: poolOwner })
+  });
+
+  it('should be able to generate,validate & execute "payment" message', async () => {
+    const { address: ldgAddress } = await ethApp.getAddress(PATH);
+    mlog.log("ledger address", ldgAddress);
+    await token.mint(ldgAddress, val1, { from: tokenOwner })
+    const message = await pool.generatePaymentMessage(ldgAddress, 200, { from: poolOwner })
+    mlog.log('message: ', message)
+
+    mlog.log("sha3(message)", web3.utils.sha3(message));
+
+    const toSign = Buffer.from(web3.utils.sha3(message).slice(2)).toString('hex');
+    mlog.log("toSign", toSign);
+    const signedLedger = await ethApp.signPersonalMessage(
+      PATH,
+      toSign,
+    );
+
+    mlog.log('signed Ledger manual', JSON.stringify(signedLedger));
+
+    let v = '0x' + signedLedger.v.toString(16);
+
+    let { r, s } = signedLedger;
+
+    const hashedToSign = hashMessage(toSign);
+
+    const recovered = web3.eth.accounts.recover({
+      messageHash: hashedToSign,
+      v,
+      r: "0x" + r,
+      s: "0x" + s
+    })
+    mlog.log("Recovered Ledger", recovered);
+
+    mlog.log("validating", `v is ${v} r is ${"0x" + r} s is ${"0x" + s}`);
+
+    assert(
+      await pool.validatePaymentMessage(
+        ldgAddress,
         200,
         v,
-        "0x" + signed.r,
-        "0x" + signed.s,
-        { from: address }
+        "0x" + signedLedger.r,
+        "0x" + signedLedger.s,
+        { from: ldgAddress }
       ),
-      "invalid signature"
+      "invalid ledger signature"
     );
   });
 });
