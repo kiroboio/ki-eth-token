e53d7e904ed76a81f4c4e6ca912db0f367e55c26
Yura Sherman <yurasherman+github@gmail.com>
2020-08-04 12:44:00 +0300

    remove trezor libs - wip

--- manual/test/trezor.js
+++ manual/test/trezor.js
@@ -62,100 +62,14 @@ contract("Trezor Test", async (accounts) => {
     mlog.log("val3           ", val3);
   });
 
-  // before("setup trezor", async () => {
-  //   const transport = await hidTransport.create();
-  //   ethApp = new App(transport);
-  //   _web3 = new web3(new LedgerProvider());
-  // });
+  before("setup trezor", async () => {
+    // start a web server
+    ;
+  });
 
-  const sanitizeHex = (hex) => {
-    hex = hex.substring(0, 2) === "0x" ? hex.substring(2) : hex;
-    if (hex == "") return "";
-    return "0x" + padLeftEven(hex);
-  };
-
-  const getBufferFromHex = (hex) => {
-    hex = sanitizeHex(hex);
-    const _hex = hex.toLowerCase().replace("0x", "");
-    return new Buffer(_hex, "hex");
-  };
-
-  const toBuffer = (v) => {
-    if (ethUtil.isHexString(v)) {
-      return ethUtil.toBuffer(v);
-    }
-    return Buffer.from(v);
-  };
-
-  const padLeftEven = (hex) => {
-    hex = hex.length % 2 != 0 ? "0" + hex : hex;
-    return hex;
-  };
-
-  const msgSigner = async (msg, path) => {
-    const result = await ethApp.signPersonalMessage(
-      path,
-      toBuffer(msg).toString("hex")
-    );
-    const v = parseInt(result.v, 10) - 27;
-    const vHex = sanitizeHex(v.toString(16));
-    // return Buffer.concat([
-    //   getBufferFromHex(result.r),
-    //   getBufferFromHex(result.s),
-    //   getBufferFromHex(vHex),
-    // ]);
-    return {
-      v: vHex,
-      r: result.r,
-      s: result.s,
-    };
-  };
 
   it("should be able to generate message", async () => {
-    const PATH = "44'/60'/0'/0/0";
-    await token.mint(pool.address, val1, { from: tokenOwner });
-    await token.mint(user1, val2, { from: tokenOwner });
-    await token.approve(pool.address, val3, { from: user1 });
-    await pool.deposit(val3, { from: user1 });
-    const res = await TrezorConnect.ethereumGetAddress({
-      path: PATH,
-      showOnTrezor: false,
-    });
-    mlog.log("trezor res", JSON.stringify(res));
-    const address = res.payload.address;
-    const message = await pool.generateAcceptTokensMessage(address, 200, {
-      from: poolOwner,
-    });
-    mlog.log("message", message);
-    mlog.log("sha3", web3.utils.sha3(message));
-    const signed = await TrezorConnect.ethereumSignMessage({
-      path: PATH,
-      message: web3.utils.sha3(message).slice(2),
-    });
 
-    mlog.log(`signed: ${JSON.stringify(signed)}`);
-
-    // let v = signed.v - 27;
-    // v = v.toString(16);
-    // if (v.length < 2) {
-    //   v = "0x0" + v;
-    // }
-
-    // let { r, s } = signed;
-
-    // mlog.log("validating", `v is ${v} r is ${"0x" + r} s is ${"0x" + s}`);
-
-    // assert(
-    //   await pool.validateAcceptTokens(
-    //     address,
-    //     200,
-    //     v,
-    //     "0x" + signed.r,
-    //     "0x" + signed.s,
-    //     { from: address }
-    //   ),
-    //   "invalid signature"
-    // );
     assert(2 === 2);
   });
 });
