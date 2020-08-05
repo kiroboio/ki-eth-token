b4fa2e1e2a2902798de08cef38a03a6a6f43daac
Yura Sherman <yurasherman+github@gmail.com>
2020-08-05 16:18:54 +0300

    use resyncTotalSupply after minting tokens

--- manual/test/trezor.js
+++ manual/test/trezor.js
@@ -106,6 +106,7 @@ contract("Trezor Test", async (accounts) => {
   it("should be able to generate, validate and execute 'token accept' message", async () => {
 
     await token.mint(pool.address, val1, { from: tokenOwner });
+    await pool.resyncTotalSupply({ from: poolOwner })
     const secret = "my secret";
     const tokens = 500;
     assert(trzAddress, 'trzAddress must be sent from the browser');
