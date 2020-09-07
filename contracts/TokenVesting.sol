a2255673c4339e943ee94f822d9f3733fd1e7ba1
Tal Asa <tal@kirobo.io>
2020-09-07 22:28:50 +0300

    vesting: configurable cancelation time + ability to release or retrieve part of the amount

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -4,53 +4,53 @@ pragma solidity ^0.6.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-/**
- * @dev A token holder contract that will allow a beneficiary to extract the
- * tokens after a given release time.
- *
- * Useful for simple vesting schedules like "advisors get all of their tokens
- * after 1 year".
- */
 contract TokenVesting {
     using SafeERC20 for IERC20;
 
-    // ERC20 basic token contract being held
     IERC20 private s_token;
-
-    // beneficiary of tokens after they are released
     address private s_beneficiary;
-
-    // timestamp when token release is enabled
     uint256 private s_releaseTime;
-
     address private s_issuer;
+    uint256 private s_retrieveTime;
 
-    constructor (IERC20 token, address beneficiary, uint256 releaseTime, address issuer) public {
-        // solhint-disable-next-line not-rely-on-time
+    constructor (IERC20 token, address beneficiary, uint256 releaseTime, address issuer, uint256 retrieveTime) public {
         require(releaseTime > block.timestamp, "TokenVesting: release time is before current time");
         s_token = token;
         s_beneficiary = beneficiary;
         s_releaseTime = releaseTime;
+        s_retrieveTime = retrieveTime;
         s_issuer = issuer;
     }
 
-    /**
-     * @return the token being held.
-     */
+    function release(uint256 amount) public {
+        require(block.timestamp >= s_releaseTime, "TokenVesting: current time is before release time");
+        require(amount > 0, "TokenVesting: no tokens to release");
+        s_token.safeTransfer(s_beneficiary, amount);
+    }
+
+    function releaseAll() public {
+        release(s_token.balanceOf(address(this)));
+    }
+
+    function retrieve(uint256 amount) public {
+        require(msg.sender == s_issuer, "TokenVesting: not issuer");
+        require(block.timestamp <= s_retrieveTime, "TokenVesting: current time is after retrieve time");
+        require(amount > 0, "TokenVesting: no tokens to retrieve");
+        s_token.safeTransfer(s_issuer, amount);
+    }
+
+    function retrieveAll() public {
+        retrieve(s_token.balanceOf(address(this)));
+    }
+
     function token() public view returns (IERC20) {
         return s_token;
     }
 
-    /**
-     * @return the beneficiary of the tokens.
-     */
     function beneficiary() public view returns (address) {
         return s_beneficiary;
     }
 
-    /**
-     * @return the time when the tokens are released.
-     */
     function releaseTime() public view returns (uint256) {
         return s_releaseTime;
     }
@@ -59,28 +59,8 @@ contract TokenVesting {
         return s_issuer;
     }
 
-    /**
-     * @notice Transfers tokens held by timelock to beneficiary.
-     */
-    function release() public virtual {
-        // solhint-disable-next-line not-rely-on-time
-        require(block.timestamp >= s_releaseTime, "TokenVesting: current time is before release time");
-
-        uint256 amount = s_token.balanceOf(address(this));
-        require(amount > 0, "TokenVesting: no tokens to release");
-
-        s_token.safeTransfer(s_beneficiary, amount);
-    }
-
-    function cancel() public virtual {
-        // solhint-disable-next-line not-rely-on-time
-        require(block.timestamp < s_releaseTime, "TokenVesting: current time is after release time");
-        require(msg.sender == s_issuer, "TokenVesting: not issuer");
-        
-        uint256 amount = s_token.balanceOf(address(this));
-        require(amount > 0, "TokenVesting: no tokens to release");
-        
-        s_token.safeTransfer(s_issuer, amount);
+    function retrieveTime() public view returns (uint256) {
+        return s_retrieveTime;
     }
 
 }
