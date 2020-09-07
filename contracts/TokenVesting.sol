f948595a29682aa3e000640c4c248f08ee5e3032
Tal Asa <tal@kirobo.io>
2020-09-07 22:33:58 +0300

    vesting: refactoring + adding events

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -13,6 +13,9 @@ contract TokenVesting {
     address private s_issuer;
     uint256 private s_retrieveTime;
 
+    event Released(uint256 amount, uint256 timestamp);
+    event Retrieved(uint256 amount, uint256 timestamp);
+
     constructor (IERC20 token, address beneficiary, uint256 releaseTime, address issuer, uint256 retrieveTime) public {
         require(releaseTime > block.timestamp, "TokenVesting: release time is before current time");
         s_token = token;
@@ -22,14 +25,19 @@ contract TokenVesting {
         s_issuer = issuer;
     }
 
+    function releaseAll() external {
+        release(s_token.balanceOf(address(this)));
+    }
+
+    function retrieveAll() external {
+        retrieve(s_token.balanceOf(address(this)));
+    }
+
     function release(uint256 amount) public {
         require(block.timestamp >= s_releaseTime, "TokenVesting: current time is before release time");
         require(amount > 0, "TokenVesting: no tokens to release");
         s_token.safeTransfer(s_beneficiary, amount);
-    }
-
-    function releaseAll() public {
-        release(s_token.balanceOf(address(this)));
+        emit Released(amount, block.timestamp);
     }
 
     function retrieve(uint256 amount) public {
@@ -37,10 +45,7 @@ contract TokenVesting {
         require(block.timestamp <= s_retrieveTime, "TokenVesting: current time is after retrieve time");
         require(amount > 0, "TokenVesting: no tokens to retrieve");
         s_token.safeTransfer(s_issuer, amount);
-    }
-
-    function retrieveAll() public {
-        retrieve(s_token.balanceOf(address(this)));
+        emit Retrieved(amount, block.timestamp);
     }
 
     function token() public view returns (IERC20) {
