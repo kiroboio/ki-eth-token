d66effe76060a18a6129dcb7c1371a5450471f91
Tal Asa <tal@kirobo.io>
2020-09-09 22:30:33 +0300

    refactoring

--- contracts/Unipool.sol
+++ contracts/Unipool.sol
@@ -6,48 +6,24 @@ import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "./Claimable.sol";
 
-contract LPTokenWrapper {
+contract Unipool is Claimable {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
-  // Uniswap v2 KIRO/ETH pair
-  IERC20 public UNI = IERC20(0xd0fd23E6924a7A34d34BC6ec6b97fadD80BE255F);
-
-  uint256 private _totalSupply;
-  mapping(address => uint256) private _balances;
-
-  function totalSupply() public view returns (uint256) {
-    return _totalSupply;
-  }
-
-  function balanceOf(address account) public view returns (uint256) {
-    return _balances[account];
-  }
-
-  function stake(uint256 amount) public virtual {
-    _totalSupply = _totalSupply.add(amount);
-    _balances[msg.sender] = _balances[msg.sender].add(amount);
-    UNI.safeTransferFrom(msg.sender, address(this), amount);
-  }
-
-  function withdraw(uint256 amount) public virtual {
-    _totalSupply = _totalSupply.sub(amount);
-    _balances[msg.sender] = _balances[msg.sender].sub(amount);
-    UNI.safeTransfer(msg.sender, amount);
-  }
-}
-
-contract Unipool is LPTokenWrapper, Claimable {
   uint256 public constant DURATION = 30 days;
   // Kirobo Token on Rinkeby
-  IERC20 public KIRO = IERC20(0xDc7988DC2fA23EA82d73B21B63Da5B905Fb52074);
+  IERC20 public constant KIRO = IERC20(0xDc7988DC2fA23EA82d73B21B63Da5B905Fb52074);
+  // Uniswap v2 KIRO/ETH pair
+  IERC20 public constant UNI = IERC20(0xd0fd23E6924a7A34d34BC6ec6b97fadD80BE255F);
 
-  uint256 public periodFinish;
-  uint256 public rewardRate;
-  uint256 public lastUpdateTime;
-  uint256 public rewardPerTokenStored;
-  mapping(address => uint256) public userRewardPerTokenPaid;
-  mapping(address => uint256) public rewards;
+  uint256 private s_totalSupply;
+  uint256 private s_periodFinish;
+  uint256 private s_rewardRate;
+  uint256 private s_lastUpdateTime;
+  uint256 private s_rewardPerTokenStored;
+  mapping(address => uint256) private s_balances;
+  mapping(address => uint256) private s_userRewardPerTokenPaid;
+  mapping(address => uint256) private s_rewards;
 
   event RewardAdded(uint256 reward);
   event Staked(address indexed user, uint256 amount);
@@ -55,11 +31,11 @@ contract Unipool is LPTokenWrapper, Claimable {
   event RewardPaid(address indexed user, uint256 reward);
 
   modifier updateReward(address account) {
-    rewardPerTokenStored = rewardPerToken();
-    lastUpdateTime = lastTimeRewardApplicable();
+    s_rewardPerTokenStored = rewardPerToken();
+    s_lastUpdateTime = lastTimeRewardApplicable();
     if (account != address(0)) {
-      rewards[account] = earned(account);
-      userRewardPerTokenPaid[account] = rewardPerTokenStored;
+      s_rewards[account] = earned(account);
+      s_userRewardPerTokenPaid[account] = s_rewardPerTokenStored;
     }
     _;
   }
@@ -71,72 +47,113 @@ contract Unipool is LPTokenWrapper, Claimable {
     require(false, "Unipool: not aceepting ether");
   }
 
-  function lastTimeRewardApplicable() public view returns (uint256) {
-    return Math.min(block.timestamp, periodFinish);
-  }
-
-  function rewardPerToken() public view returns (uint256) {
-    if (totalSupply() == 0) {
-      return rewardPerTokenStored;
+  function addReward(address from, uint256 amount) external onlyOwner() updateReward(address(0)) {
+    require(amount > 0, 'Unipool: Cannot approve 0');
+    if (block.timestamp >= s_periodFinish) {
+      s_rewardRate = amount.div(DURATION);
+    } else {
+      uint256 remaining = s_periodFinish.sub(block.timestamp);
+      uint256 leftover = remaining.mul(s_rewardRate);
+      s_rewardRate = amount.add(leftover).div(DURATION);
     }
-    return
-      rewardPerTokenStored.add(
-        lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(totalSupply())
-      );
+    s_lastUpdateTime = block.timestamp;
+    s_periodFinish = block.timestamp.add(DURATION);
+    KIRO.safeTransferFrom(from, address(this), amount);
+    emit RewardAdded(amount);
   }
 
-  function earned(address account) public view returns (uint256) {
-    return
-      balanceOf(account).mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(
-        rewards[account]
-      );
-  }
-
-  // stake visibility is public as overriding LPTokenWrapper's stake() function
-  function stake(uint256 amount) public updateReward(msg.sender) override {
-    require(amount > 0, 'Cannot stake 0');
-    super.stake(amount);
+  function stake(uint256 amount) external updateReward(msg.sender) {
+    require(amount > 0, 'Unipool: Cannot stake 0');
+    s_totalSupply = s_totalSupply.add(amount);
+    s_balances[msg.sender] = s_balances[msg.sender].add(amount);
+    UNI.safeTransferFrom(msg.sender, address(this), amount);
     emit Staked(msg.sender, amount);
   }
 
-  function withdraw(uint256 amount) public updateReward(msg.sender) override {
-    require(amount > 0, 'Cannot withdraw 0');
-    super.withdraw(amount);
-    emit Withdrawn(msg.sender, amount);
+  function exit() external {
+    withdraw(s_balances[msg.sender]);
+    getReward();
   }
 
-  function exit() external {
-    withdraw(balanceOf(msg.sender));
-    getReward();
+  function withdraw(uint256 amount) public updateReward(msg.sender) {
+    require(amount > 0, 'Unipool: Cannot withdraw 0');
+    s_totalSupply = s_totalSupply.sub(amount);
+    s_balances[msg.sender] = s_balances[msg.sender].sub(amount);
+    UNI.safeTransfer(msg.sender, amount);
+    emit Withdrawn(msg.sender, amount);
   }
 
   function getReward() public updateReward(msg.sender) {
     uint256 reward = earned(msg.sender);
     if (reward > 0) {
-      rewards[msg.sender] = 0;
+      s_rewards[msg.sender] = 0;
       KIRO.safeTransfer(msg.sender, reward);
       emit RewardPaid(msg.sender, reward);
     }
   }
 
-  function addReward(
-    address _from,
-    uint256 _amount
-  ) external onlyOwner() updateReward(address(0)) {
-    require(_amount > 0, 'Cannot approve 0');
-
-    if (block.timestamp >= periodFinish) {
-      rewardRate = _amount.div(DURATION);
-    } else {
-      uint256 remaining = periodFinish.sub(block.timestamp);
-      uint256 leftover = remaining.mul(rewardRate);
-      rewardRate = _amount.add(leftover).div(DURATION);
-    }
-    lastUpdateTime = block.timestamp;
-    periodFinish = block.timestamp.add(DURATION);
-
-    KIRO.safeTransferFrom(_from, address(this), _amount);
-
-    emit RewardAdded(_amount);
+  function lastTimeRewardApplicable() public view returns (uint256) {
+    return Math.min(block.timestamp, s_periodFinish);
   }
+
+  function rewardPerToken() public view returns (uint256) {
+    if (s_totalSupply == 0) {
+      return s_rewardPerTokenStored;
+    }
+    return
+      s_rewardPerTokenStored.add
+      (
+        lastTimeRewardApplicable()
+        .sub(s_lastUpdateTime)
+        .mul(s_rewardRate)
+        .mul(1e18)
+        .div(s_totalSupply)
+      );
+  }
+
+  function earned(address account) public view returns (uint256) {
+    return
+    (
+      s_balances[account].mul
+      (
+        rewardPerToken()
+        .sub(s_userRewardPerTokenPaid[account])
+      )
+      .div(1e18)
+      .add(s_rewards[account])
+    );
+  }
+
+  function totalSupply() external view returns (uint256) {
+    return s_totalSupply;
+  }
+
+  function periodFinish() external view returns (uint256) {
+    return s_periodFinish;
+  }
+
+  function rewardRate() external view returns (uint256) {
+    return s_rewardRate;
+  }
+
+  function lastUpdateTime() external view returns (uint256) {
+    return s_lastUpdateTime;
+  }
+
+  function rewardPerTokenStored() external view returns (uint256) {
+    return s_rewardPerTokenStored;
+  }
+
+  function balanceOf(address account) external view returns (uint256) {
+    return s_balances[account];
+  }
+
+  function userRewardPerTokenPaid(address account) external view returns (uint256) {
+    return s_userRewardPerTokenPaid[account];
+  }
+
+  function rewards(address account) external view returns (uint256) {
+    return s_rewards[account];
+  }
+
 }
