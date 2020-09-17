0f69f92625a096702319a5658e857ca442a8bc6b
Tal Asa <email.tal.asa@gmail.com>
2020-09-17 20:19:26 +0300

    Unipool.sol --> Staking.sol

--- scripts/build-uniswap.sh
+++ scripts/build-uniswap.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPTPATH
+pushd $SCRIPTPATH
 cp truffle-config.js $SCRIPTPATH/../node_modules/@uniswap/v2-core/
-cd $SCRIPTPATH/../node_modules/@uniswap/v2-core/
+pushd $SCRIPTPATH/../node_modules/@uniswap/v2-core/
 rm -rf build
 truffle build
+popd
+popd
